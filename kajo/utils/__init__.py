5ee96e49ebd6c2f600b3f6754bca832eefa05060
deko <dkolokol@gmail.com>
2020-03-04 16:50:09 +0100

    change folder structure for setup

--- kajo/utils/__init__.py
+++ kajo/utils/__init__.py
@@ -3,12 +3,10 @@
 import os
 import sys
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(BASE_DIR)
 if PROJECT_DIR not in sys.path:
     sys.path.append(PROJECT_DIR)
-
 
 from .containers import flatten_list, flatten_dict, \
      ensure_dict, ensure_list, \
