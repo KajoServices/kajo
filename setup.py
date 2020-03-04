29291328ef5470c183870c0d5d68c0b8401cddff
deko <dkolokol@gmail.com>
2020-03-04 16:14:04 +0100

    file for setuptools

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='kajo',
+    version='0.0.1',
+    description='KAJO services utils, contributions, etc.',
+    url='git@github.com:KajoServices/kajo.git',
+    author='Denis Kolokol',
+    author_email='denis.kolokol@kajoservices.com',
+    license='unlicense',
+    packages=['kajo'],
+    zip_safe=False
+)
